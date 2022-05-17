58dc3abf75bd40806495c4d852e8f374274aa37b
guoconde <guoconde@gmail.com>
2022-05-17 13:59:09 -0300

    Feat: add getitembyid item on item route

--- src/routes/backlogRoutes/itemsRoute.ts
+++ src/routes/backlogRoutes/itemsRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import * as itemsController from '../../controllers/backlogController/itemsController.js';
+import { tokenMiddleware } from '../../middlewares/tokenMiddleware.js';
 
 const itemsRouter = Router();
 
-itemsRouter.get('/items', itemsController.getItems);
-itemsRouter.post('/items/new', itemsController.insterItem);
-itemsRouter.put('/items/:id', itemsController.updateItem);
-itemsRouter.delete('/items/:id', itemsController.deleteItem);
-itemsRouter.get('/items/:id', itemsController.getItemById);
+itemsRouter.get('/items', tokenMiddleware, itemsController.getItems);
+itemsRouter.post('/items/new', tokenMiddleware, itemsController.insterItem);
+itemsRouter.put('/items/:id', tokenMiddleware, itemsController.updateItem);
+itemsRouter.delete('/items/:id', tokenMiddleware, itemsController.deleteItem);
+itemsRouter.get('/items/:id', tokenMiddleware, itemsController.getItemById);
 
 export default itemsRouter;
